b6ef8bb15fc96afc38a9d2f6a744d10c257b20f4
ttossavainen <teemu.tossavainen@hiq.fi>
2023-01-26 14:09:03 +0200

    Result update

--- Frends.MicrosoftSQL.BulkInsert/Frends.MicrosoftSQL.BulkInsert.Tests/UnitTests.cs
+++ Frends.MicrosoftSQL.BulkInsert/Frends.MicrosoftSQL.BulkInsert.Tests/UnitTests.cs
@@ -69,7 +69,7 @@ public class UnitTests
 
     [TestMethod]
     public async Task TestBulkInsert_FireTriggers()
-    {
+    { 
         var transactionLevels = new List<SqlTransactionIsolationLevel>() {
             SqlTransactionIsolationLevel.Unspecified,
             SqlTransactionIsolationLevel.Serializable,
