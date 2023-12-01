cfcea276df680aca1f8e03aa49bfd50b6be5675d
Riku Virtanen <virtanen.riku91@gmail.com>
2023-12-01 07:53:50 +0200

    Linting fixes and added Tests for code coverage

--- Frends.MicrosoftSQL.ExecuteQuery/Frends.MicrosoftSQL.ExecuteQuery.Tests/AutoUnitTests.cs
+++ Frends.MicrosoftSQL.ExecuteQuery/Frends.MicrosoftSQL.ExecuteQuery.Tests/AutoUnitTests.cs
@@ -20,7 +20,7 @@ public class AutoUnitTests
 
     private static readonly string _connString = "Server=127.0.0.1,1433;Database=Master;User Id=SA;Password=Salakala123!";
     private static readonly string _tableName = "TestTable";
-    
+
     [TestInitialize]
     public void Init()
     {
