79530387ef786a9a282cdaeccbb8e06ce2f4acde
FrendsSarlinS <sampo.sarlin@frends.com>
2024-08-07 15:25:42 +0300

    Test added

--- Frends.MicrosoftSQL.BatchOperation/Frends.MicrosoftSQL.BatchOperation.Tests/ExceptionUnitTests.cs
+++ Frends.MicrosoftSQL.BatchOperation/Frends.MicrosoftSQL.BatchOperation.Tests/ExceptionUnitTests.cs
@@ -50,4 +50,50 @@ public class ExceptionUnitTests
         Assert.IsTrue(result.ErrorMessage.Contains("Login failed for user 'SA'."));
         Assert.AreEqual(0, result.RecordsAffected);
     }
+
+    [TestMethod]
+    public async Task TestBatchOperation_ExecuteHandler_Exception_TransactionIsNull()
+    {
+        // Arrange
+        var input = new Input
+        {
+            ConnectionString = "Server=127.0.0.1,1433;Database=Master;User Id=SA;Password=Salakala123!;TrustServerCertificate=True",
+        };
+
+        var options = new Options
+        {
+            ThrowErrorOnFailure = true,
+            SqlTransactionIsolationLevel = SqlTransactionIsolationLevel.None,
+            CommandTimeoutSeconds = 60,
+        };
+
+        // Act
+        var ex = await Assert.ThrowsExceptionAsync<Exception>(() => MicrosoftSQL.BatchOperation(input, options, CancellationToken.None));
+        Console.WriteLine("Actual exception message: " + ex.Message);
+
+        // Assert
+        Assert.IsNotNull(ex.InnerException);
+        Assert.IsTrue(ex.InnerException.Message.Contains("ExecuteHandler exception: 'Options.SqlTransactionIsolationLevel = None', so there was no transaction rollback."));
+    }
+
+    [TestMethod]
+    public async Task TestBatchOperation_ExecuteHandler_Exception_TransactionIsNotNull()
+    {
+        var input = new Input
+        {
+            ConnectionString = "Server=127.0.0.1,1433;Database=Master;User Id=SA;Password=Salakala123!;TrustServerCertificate=True",
+        };
+
+        var options = new Options
+        {
+            ThrowErrorOnFailure = true,
+            SqlTransactionIsolationLevel = SqlTransactionIsolationLevel.ReadCommitted,
+            CommandTimeoutSeconds = 60,
+        };
+
+        var ex = await Assert.ThrowsExceptionAsync<Exception>(() => MicrosoftSQL.BatchOperation(input, options, CancellationToken.None));
+
+        Assert.IsNotNull(ex.InnerException);
+        Assert.IsTrue(ex.InnerException.Message.Contains("ExecuteHandler exception: (If required) transaction rollback completed without exception."));
+    }
 }
